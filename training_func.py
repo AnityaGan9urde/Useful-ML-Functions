2ee77a3c0d579536070a663fbfa4db16cdeb3a56
Anitya Gangurde <anityagangurde01@gmail.com>
2021-10-16 13:37:19 +0530

    Add files via upload

---
+++ training_func.py
@@ -0,0 +1,45 @@
+'''
+    Code to train various ML models and apply cross validation and create a table with results for each model
+'''
+# selection of algorithms to consider and set performance measure
+models = []
+models.append(('Logistic Regression', LogisticRegression(solver='liblinear', random_state=7,
+                                                         class_weight='balanced')))
+models.append(('Random Forest', RandomForestClassifier(
+    n_estimators=100, random_state=7)))
+models.append(('SVM', SVC(gamma='auto', random_state=7)))
+models.append(('KNN', KNeighborsClassifier()))
+models.append(('Decision Tree Classifier',
+               DecisionTreeClassifier(random_state=7)))
+models.append(('Gaussian NB', GaussianNB()))
+
+acc_results = []
+auc_results = []
+names = []
+# set table to table to populate with performance results
+col = ['Algorithm', 'ROC AUC Mean', 'ROC AUC STD', 
+       'Accuracy Mean', 'Accuracy STD']
+df_results = pd.DataFrame(columns=col)
+i = 0
+# evaluate each model using cross-validation
+for name, model in models:
+    kfold = model_selection.KFold(
+        n_splits=10, random_state=7)  # 10-fold cross-validation
+
+    cv_acc_results = model_selection.cross_val_score(  # accuracy scoring
+        model, X_train, y_train, cv=kfold, scoring='accuracy')
+
+    cv_auc_results = model_selection.cross_val_score(  # roc_auc scoring
+        model, X_train, y_train, cv=kfold, scoring='roc_auc')
+
+    acc_results.append(cv_acc_results)
+    auc_results.append(cv_auc_results)
+    names.append(name)
+    df_results.loc[i] = [name,
+                         round(cv_auc_results.mean()*100, 2),
+                         round(cv_auc_results.std()*100, 2),
+                         round(cv_acc_results.mean()*100, 2),
+                         round(cv_acc_results.std()*100, 2)
+                         ]
+    i += 1
+df_results.sort_values(by=['ROC AUC Mean'], ascending=False)
